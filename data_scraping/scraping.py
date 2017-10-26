5e368d603dc19ef96b2df55b448ef9c76da03364
DeqingQu <qu.deqing@gmail.com>
2017-10-25 20:30:29 -0700

    record data to sql

--- data_scraping/scraping.py
+++ data_scraping/scraping.py
@@ -1,8 +1,8 @@
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
-import json
 import mysql.connector
+from datetime import datetime
 
 
 #   retrieve data from URL
@@ -48,11 +48,28 @@ urls = [
     "https://www.amazon.com/Philips-AVENT-Natural-Glass-Bottle/dp/B00PF83R0W/ref=sr_1_6_s_it?s=baby-products&ie=UTF8&qid=1508883080&sr=1-6&keywords=philips%2Bavent&th=1",
     "https://www.amazon.com/Britax-Boulevard-G4-1-Convertible-Domino/dp/B00OLRKNGY/ref=sr_1_1_s_it?s=baby-products&ie=UTF8&qid=1508884406&sr=1-1&refinements=p_89%3ABritax%2BUSA&th=1",
     "https://www.amazon.com/Bose-QuietComfort-Wireless-Headphones-Cancelling/dp/B01E3SNO1G/ref=sr_1_3?s=electronics&ie=UTF8&qid=1508884685&sr=1-3&keywords=bose",
-    "https://www.amazon.com/JBL-Wireless-Bluetooth-Speaker-Pairing/dp/B00GOF0ZQ4/ref=sr_1_5?ie=UTF8&qid=1508884897&sr=8-5&keywords=jbl+pulse",
-    "https://www.amazon.com/JBPortable-Splashproof-Bluetooth-Speaker/dp/B0147K"
+    "https://www.amazon.com/JBL-Wireless-Bluetooth-Speaker-Pairing/dp/B00GOF0ZQ4/ref=sr_1_5?ie=UTF8&qid=1508884897&sr=8-5&keywords=jbl+pulse"
 ]
+
+connection = mysql.connector.connect(host="localhost", port=3306, user="demo", passwd="demo", db="semdemo")
+db = connection.cursor(prepared=True)
+
+db.execute("""
+        CREATE TABLE IF NOT EXISTS commodity_price_record (
+            rid MEDIUMINT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(256) NOT NULL DEFAULT '',
+            price FLOAT NOT NULL DEFAULT 0.0,
+            url VARCHAR(256) NOT NULL DEFAULT '',
+            date VARCHAR(256) NOT NULL DEFAULT '',
+            CONSTRAINT UC_URL_Date UNIQUE (url,date)
+        )""")
 
 for url in urls:
     commodity = Commodity(url)
     commodity.retrieve_info()
     commodity.print_commodity()
+    db.execute("REPLACE INTO commodity_price_record(title,price,url,date) values(?,?,?,?)",
+               [commodity.title, commodity.price, commodity.url, datetime.now().strftime("%Y-%m-%d")])
+
+connection.commit()  # required, as mysql generally doesn't autocommit
+connection.close()
