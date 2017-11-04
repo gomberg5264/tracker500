78ac9194952caadeb65b5bf8b6642b22d78733b6
DeqingQu <qu.deqing@gmail.com>
2017-11-04 15:50:34 -0700

    add header in request to avoid robot check

--- data_scraping/scraping_test.py
+++ data_scraping/scraping_test.py
@@ -1,6 +1,7 @@
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
+import random
 import mysql.connector
 from datetime import datetime
 
@@ -10,7 +11,16 @@ def retrieve(url: str):
     #   retrieves content at the specified url
     print("*", url)
     sleep(1)  # *never* web scrape faster than 1 request per second
-    r = requests.get(url, verify=False)  # get the HTML; ignore SSL errors (present on this particular site)
+    UAS = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+           "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
+           "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+           "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+           )
+    ua = UAS[random.randrange(len(UAS))]
+    headers = {'user-agent': ua}
+    r = requests.get(url, headers=headers, verify=False)  # get the HTML; ignore SSL errors (present on this particular site)
     soup = BeautifulSoup(r.text, "lxml")  # parse the HTML
     return soup
 
