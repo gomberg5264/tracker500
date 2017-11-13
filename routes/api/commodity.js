36d8726a496de9ee6f628d6868d5b37cec0942f1
DeqingQu <qu.deqing@gmail.com>
2017-11-13 08:31:36 -0800

    add new version api without limitation of date, need to refactor!

--- routes/api/commodity.js
+++ routes/api/commodity.js
@@ -49,4 +49,46 @@ router.get('/:c_id/:start_date/:end_date', function(req, res) {
     }, c_id, start_date, end_date);
 });
 
+/**
+ * query commodity prices in all time period
+ * GET Method
+ * @param {string} c_id
+ * @return {object} {"title": "Bose QC35", "price_array": [{"price": 300, "date": "2017-10-29"}, {...}...]}
+ */
+router.get('/:c_id', function(req, res) {
+    // make sure we end with a slash, so that relative links point *into* this router
+    if (req.originalUrl.slice(-1) != '/') {
+      console.log("Output originalUrl" + req.originalUrl);
+      return res.redirect(req.originalUrl + '/');
+    }
+
+    var c_id = req.params['c_id'];
+
+    commodity_models.quertPrices(dbcfg, function(err, data) {
+
+        console.log("data_length = " + data.length);
+
+        var result = {};
+        if (data.length === 0) {
+            result['title'] = "";
+            result['price_array'] = [];
+        }
+        else {
+            result['title'] = data[0]['c_title'];
+            result['price_array'] = [];
+            for(i in data) {
+                var item = {};
+                item['price'] = data[i]['c_price'];
+                item['date'] = data[i]['r_date'];
+                result['price_array'].push(item);
+            }
+        }
+
+        console.log(result);
+
+        res.end(JSON.stringify(result));
+
+    }, c_id);
+});
+
 module.exports = router;
--- test/api_commodity_test.js
+++ test/api_commodity_test.js
@@ -9,8 +9,26 @@ var start_date = "2017-11-3";
 var end_date = "2017-11-10";
 
 describe("test site with superagent", () => {
-    it("test GET /url/api/commodity/", (done) => {
+    it("test GET /url/api/commodity with limitation of date", (done) => {
         superagent.get(BASE_URL + '/commodity/' + c_id + '/' + start_date + "/" + end_date)
+            .end(function(err, res) {
+
+                // expect(err).to.not.exist;
+                expect(res).to.exist;
+                expect(res.status).to.equal(200);
+                expect(res.text).to.exist;
+
+                var commodity = JSON.parse(res.text);
+                expect(commodity).to.be.an.instanceof(Object);
+                expect(commodity['title']).to.be.a('string');
+                expect(commodity['price_array']).to.be.an.instanceof(Array);
+
+                done();
+            });
+    });
+
+    it("test GET /url/api/commodity/ without limitation of date", (done) => {
+        superagent.get(BASE_URL + '/commodity/' + c_id)
             .end(function(err, res) {
 
                 // expect(err).to.not.exist;
