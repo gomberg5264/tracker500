09dd8eb2dcb472c1be25691c7a5c6f4fe3c7e392
DeqingQu <qu.deqing@gmail.com>
2017-11-15 15:36:18 -0800

    refactor frontend js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,30 +1,33 @@
+function submitUrl() {
+    var url_value = $("#url_input").val();
+    if(url_value === "" || url_value === "http://") {
+        alert("No parameter");
+    }
+    else {
+        $.ajax({
+            type: "post",
+            url:"/api/urls/",
+            async: true,
+            dataType:"json",
+            data:{"url":url_value},
+            success: function(data) {
+                alert("Success");
+                $("#url_input").val("");
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert(XMLHttpRequest.status);
+                alert(XMLHttpRequest.readyState);
+                alert(textStatus);
+            }
+        });
+    }
+}
 
 $('document').ready(function(){
 
     $("#url_submit_btn").click(function() {
 
-        var url_value = $("#url_input").val();
-        if(url_value === "" || url_value === "http://") {
-            alert("No parameter");
-        }
-        else {
-            $.ajax({
-                type: "post",
-                url:"/api/urls/",
-                async: true,
-                dataType:"json",
-                data:{"url":url_value},
-                success: function(data) {
-                    alert("Success");
-                    $("#url_input").val("");
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    alert(XMLHttpRequest.status);
-                    alert(XMLHttpRequest.readyState);
-                    alert(textStatus);
-                }
-            });
-        }
+        submitUrl();
     });
 
 });
