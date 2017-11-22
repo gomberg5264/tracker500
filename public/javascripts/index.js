dbdc35e9dcf83827fb2284e0f0cbe6b24e84db09
DeqingQu <qu.deqing@gmail.com>
2017-11-21 18:39:22 -0800

    optimize UI and add awesome font

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -54,8 +54,12 @@ $('document').ready(function(){
         for (i in data) {
             var urlObj = data[i];
             console.log(urlObj);
-            $('.row').append("<div class='col-md-9 col-md-offset-3'> ID: " + urlObj['c_id']
-                + ", URL: " + urlObj['c_url'] + "</div>");
+
+            var url_id_td = "<td class='url_id'>" + urlObj['c_id'] + "</td>";
+            var url_url_td = "<td class='url_url'>" + urlObj['c_url'] + "</td>";
+            var update_td = "<td><a href='#'><i class='fa fa-pencil' aria-hidden='true'></i>&nbsp; Update</a></td>";
+            var delete_td = "<td><a href='#'><i class='fa fa-trash' aria-hidden='true'></i>&nbsp; Delete</a></td>";
+            $('#url_tbody').append("<tr>" + url_id_td + url_url_td + update_td + delete_td + "</tr>")
         }
     });
 
