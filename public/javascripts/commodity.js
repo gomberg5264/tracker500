8f898f9b8547433ee3f8f78a61cf2d33daa0c433
DeqingQu <qu.deqing@gmail.com>
2017-11-24 10:20:02 -0800

    commodity price page

--- public/javascripts/commodity.js
+++ public/javascripts/commodity.js
@@ -20,9 +20,19 @@ $('document').ready(function(){
 
     var commodity_id = $('.hidden_id').text();
     listAllPrices(commodity_id, (data)=>{
-        for (i in data) {
-            var price = data[i];
-            console.log(price);
+
+        var title = data['title'];
+        var prices = data['prices'];
+
+        $("h3").text(title);
+
+        for (i in prices) {
+            var priceObj = prices[i];
+
+            var commodity_price_td = "<td>" + priceObj['price'] + "</td>"
+            var commodity_date_td = "<td>" + priceObj['date'] + "</td>"
+            $('#commodity_tbody').append("<tr>" + commodity_price_td + commodity_date_td + "</tr>");
+
             // var url_id_td = "<td class='urllist_id'>" + urlObj['c_id'] + "</td>";
             // var url_url_td = "<td class='urllist_url'>" + urlObj['c_url'] + "</td>";
             // var update_td = "<td><a href='#'><i class='fa fa-pencil' aria-hidden='true'></i>&nbsp; Update</a></td>";
