5625c2e3ba696ced9be7ca11bbbc059d4472b6cb
DeqingQu <qu.deqing@gmail.com>
2017-12-04 08:28:49 -0800

    remove duplicated functions in commodity_model

--- models/commodity.js
+++ models/commodity.js
@@ -42,54 +42,7 @@ function queryPrices(dbcfg, callback, c_id, start_date, end_date) {
     db.stage(dbcfg).query(query_str).finale(callback);
 }
 
-
-//
-//  description: insert a url to DB
-//  param: url is string
-//  return callback(err, result), where result is an object, containing 'c_id' and 'c_url'
-//
-function insertUrl(dbcfg, url, callback) {
-    var stage = db.stage(dbcfg);
-    stage.execute(`
-        INSERT INTO commodity_url(c_url) VALUES(?)
-        ON DUPLICATE KEY UPDATE c_url=?
-        `, [url, url]);
-
-    stage.queryInt("select LAST_INSERT_ID()");
-    stage.finale((err, results) => {
-        console.log("output results :" + JSON.stringify(results));  //  insert op return 1, select op return id
-        if (err)
-            return callback(err);
-        else if (results.length != 2)
-            return callback("Internal error: incorrect number of results returned", null);
-        else
-            return callback(err, {
-                "c_id": results[1],
-                "c_url": url
-        });
-    });
-}
-
-//
-//  description: list all urls in DB, and order by url_id
-//
-function listAllUrls(dbcfg, callback) {
-    /// issue a single query, then output the result to callback(err, results)
-    db.stage(dbcfg).query("SELECT * FROM commodity_url ORDER BY c_id").finale(callback);
-}
-
-//
-//  description: delete the url with specified url
-//
-function deleteUrl(dbcfg, url, callback) {
-    db.stage(dbcfg).execute(
-        "DELETE FROM commodity_url WHERE c_url=?", [url]).finale(callback);
-}
-
 module.exports = {
   init: init,
-  queryPrices: queryPrices,
-  insertUrl: insertUrl,
-  listAllUrls: listAllUrls,
-  deleteUrl:deleteUrl
+  queryPrices: queryPrices
 };
