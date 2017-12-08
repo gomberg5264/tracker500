cb44288f9e64d277777bcc022627fa9c7636ba23
DeqingQu <qu.deqing@gmail.com>
2017-12-07 23:24:08 -0800

    change update href to be a button

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -92,9 +92,10 @@ $('document').ready(function(){
 
             var url_id_td = "<td class='commodity_id'><a href='/commodity/" + urlObj['c_id'] + "'>" + urlObj['c_id'] + "</a></td>";
             var url_url_td = "<td class='commodity_url'><a href='" + urlObj['c_url'] + "'>" + urlObj['c_url'] + "</a></td>";
-            var update_td = "<td class='update_url_btn'><a href='#'><i class='fa fa-pencil' aria-hidden='true'></i>&nbsp; Update</a></td>";
+            var update_td = "<td><button class='btn btn-primary update_url_btn' type='submit' id='update_btn_" + urlObj['c_id'] + "'>update</button></td>";
+            // var update_td = "<td class='update_url_btn'><a href='#'><i class='fa fa-pencil' aria-hidden='true'></i>&nbsp; Update</a></td>";
             // var delete_td = "<td class='delete_url_btn'><a href='#'><i class='fa fa-trash' aria-hidden='true'></i>&nbsp; Delete</a></td>";
-            var delete_td = "<td><button class='btn btn-primary delete_url_btn' type='submit' id='delete_btn_" + urlObj['c_id'] + "'>delete</button></td>"
+            var delete_td = "<td><button class='btn btn-primary delete_url_btn' type='submit' id='delete_btn_" + urlObj['c_id'] + "'>delete</button></td>";
             $('#url_tbody').append("<tr id='tr_" + urlObj['c_id'] + "'>" + url_id_td + url_url_td + update_td + delete_td + "</tr>")
         }
 
@@ -117,8 +118,8 @@ $('document').ready(function(){
 
         $('.update_url_btn').on('click', function(){
 
-            var commodity_url = $(this).prev().text();
-            var commodity_id = $(this).prev().prev().text();
+            var commodity_url = $(this).parent().prev().text();
+            var commodity_id = $(this).attr('id').substring("update_btn_".length);
             var msg = "id = " + commodity_id + ", url =" + commodity_url;
             alert(msg);
         });
