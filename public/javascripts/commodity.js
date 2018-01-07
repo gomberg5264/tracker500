293b19ac98fb56b460ed2bf852a4461ee5e0bc22
DeqingQu <qu.deqing@gmail.com>
2018-01-06 21:37:24 -0800

    draw a chart template

--- public/javascripts/commodity.js
+++ public/javascripts/commodity.js
@@ -16,6 +16,59 @@ function listAllPrices(url_id, callback) {
     });
 }
 
+function createConfig(details, data) {
+	return {
+		type: 'line',
+		data: {
+			labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+			datasets: [{
+				label: details.label,
+				steppedLine: details.steppedLine,
+        		data: data,
+				borderColor: details.color,
+				fill: false,
+			}]
+		},
+		options: {
+			responsive: true,
+			title: {
+				display: true,
+				text: details.label,
+			}
+		}
+	};
+}
+
+function drawChart() {
+    var container = document.querySelector('.container');
+
+    var data = [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+    ];
+
+    var setting = {
+        steppedLine: true,
+        label: 'Step Before Interpolation',
+        color: window.chartColors.green
+    };
+
+    var div = document.createElement('div');
+    div.classList.add('chart-container');
+
+    var canvas = document.createElement('canvas');
+    div.appendChild(canvas);
+    container.appendChild(div);
+
+    var ctx = canvas.getContext('2d');
+    var config = createConfig(setting, data);
+    new Chart(ctx, config);
+}
+
 $('document').ready(function(){
 
     var commodity_id = $('.hidden_id').text();
@@ -40,73 +93,5 @@ $('document').ready(function(){
         }
     });
 
+    drawChart();
 });
-
-
-	function createConfig(details, data) {
-		return {
-			type: 'line',
-			data: {
-				labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
-				datasets: [{
-					label: 'steppedLine: ' + ((typeof(details.steppedLine) === 'boolean') ? details.steppedLine : `'${details.steppedLine}'`),
-					steppedLine: details.steppedLine,
-					data: data,
-					borderColor: details.color,
-					fill: false,
-				}]
-			},
-			options: {
-				responsive: true,
-				title: {
-					display: true,
-					text: details.label,
-				}
-			}
-		};
-	}
-
-
-	window.onload = function() {
-		var container = document.querySelector('.container');
-
-		var data = [
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor(),
-			randomScalingFactor()
-		];
-
-		var steppedLineSettings = [{
-			steppedLine: false,
-			label: 'No Step Interpolation',
-			color: window.chartColors.red
-		}, {
-			steppedLine: true,
-			label: 'Step Before Interpolation',
-			color: window.chartColors.green
-		}, {
-			steppedLine: 'before',
-			label: 'Step Before Interpolation',
-			color: window.chartColors.green
-		}, {
-			steppedLine: 'after',
-			label: 'Step After Interpolation',
-			color: window.chartColors.purple
-		}];
-
-		steppedLineSettings.forEach(function(details) {
-			var div = document.createElement('div');
-			div.classList.add('chart-container');
-
-			var canvas = document.createElement('canvas');
-			div.appendChild(canvas);
-			container.appendChild(div);
-
-			var ctx = canvas.getContext('2d');
-			var config = createConfig(details, data);
-			new Chart(ctx, config);
-		});
-	};
