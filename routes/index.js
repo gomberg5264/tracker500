42d7cb21b611a34bf8ca69b20ec0a2ace802c128
DeqingQu <qu.deqing@gmail.com>
2018-09-17 22:16:28 -0700

    typo correction

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Amazon Prize Tracker' });
+  res.render('index', { title: 'Amazon Price Tracker' });
 });
 
 module.exports = router;
